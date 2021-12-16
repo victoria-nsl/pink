ad2a1f92f19132a30f82f1f64e1e53df700f5d1c
victoria-nsl <vbnaish@yandex.ru>
2021-12-16 18:00:58 +0300

    Добавляет скрипт для лайков

---
+++ source/js/modules/photo-like.js
@@ -0,0 +1,24 @@
+const blockPhoto = document.querySelector('.photo');
+const buttonsLike = blockPhoto.querySelectorAll('.photo__button-like');
+const numbersLike = blockPhoto.querySelectorAll('.photo__like span');
+
+
+const showLike = (button, counter) => {
+  let quantity = +numbersLike[counter].textContent;
+  if (!button.classList.contains('photo__button-like--active')) {
+    button.classList.add('photo__button-like--active');
+    quantity += 1;
+  } else {
+    button.classList.remove('photo__button-like--active');
+    quantity -= 1;
+  }
+  numbersLike[counter].textContent = quantity;
+};
+
+if (blockPhoto) {
+  buttonsLike.forEach((buttonLike, index) => {
+    buttonLike.addEventListener('click', () => {
+      showLike(buttonLike, index);
+    });
+  });
+}
