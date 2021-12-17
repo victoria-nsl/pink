5238430fd6b8eaea5a5bd02416c0f3da942d74f7
victoria-nsl <vbnaish@yandex.ru>
2021-12-17 14:31:44 +0300

    Корректирует код для бегунков

--- source/js/modules/photo-settings.js
+++ source/js/modules/photo-settings.js
@@ -7,20 +7,22 @@ const settings =formSetting.querySelectorAll('.user-post__item-setting');
 
 
 const changeSetting = (button, counter) => {
-  switchesSetting.forEach((switchSetting) => {
-    if (switchSetting.classList.contains('user-post__button-setting--active')) {
-      switchSetting.classList.remove('user-post__button-setting--active');
-    }
-  });
-
   settings.forEach((setting) => {
     if (setting.classList.contains('user-post__item-setting--active')) {
       setting.classList.remove('user-post__item-setting--active');
     }
   });
 
-  button.classList.add('user-post__button-setting--active');
+  switchesSetting.forEach((switchSetting) => {
+    if (switchSetting.classList.contains('user-post__button-setting--active')) {
+      switchSetting.classList.remove('user-post__button-setting--active');
+      switchSetting.disabled = false;
+    }
+  });
+
   settings[counter].classList.add('user-post__item-setting--active');
+  button.classList.add('user-post__button-setting--active');
+  button.disabled = true;
 };
 
 
@@ -28,16 +30,19 @@ if (formSetting) {
   if (page.clientWidth < WIDTH_TABLET) {
 
     settings.forEach((setting) => {
-      if (setting.classList.contains('user-post__item-setting--nojs')) {
-        setting.classList.remove('user-post__item-setting--nojs');
-      }
+      setting.classList.remove('user-post__item-setting--active');
+      setting.classList.remove('user-post__item-setting--nojs');
     });
 
     switchesSetting.forEach((switchSetting) => {
-      if (switchSetting.classList.contains('user-post__button-setting--nojs')) {
-        switchSetting.classList.remove('user-post__button-setting--nojs');
-      }
+      switchSetting.classList.remove('user-post__button-setting--active');
+      switchSetting.classList.remove('user-post__button-setting--nojs');
+      switchSetting.disabled = false;
     });
+
+    settings[0].classList.add('user-post__item-setting--active');
+    switchesSetting[0].classList.add('user-post__button-setting--active');
+    switchesSetting[0].disabled = true;
 
     switchesSetting.forEach((switchSetting, index) => {
       switchSetting.addEventListener('click', () => {
