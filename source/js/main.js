e0fee89e3b0f26d708f41faaa7dfc936b83d77c7
victoria-nsl <vbnaish@yandex.ru>
2021-12-24 14:57:00 +0300

    Добавляет верстку раздела Отзывы

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,4 @@ import './modules/navigation.js';
 import './modules/photo-upload.js';
 import './modules/photo-settings.js';
 import './modules/photo-like.js';
+import './modules/slider-feedback.js';
---
+++ source/js/modules/slider-feedback.js
@@ -0,0 +1,22 @@
+window.$(document).ready(() => {
+  window.$('.feedback__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    draggable:false,
+    swipe: false,
+
+    responsive:[
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: true,
+          swipe: true,
+        },
+      },
+    ],
+  });
+});
