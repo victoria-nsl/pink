c242d9fa29563ac7e2a58bf176455d24a49ce8ed
victoria-nsl <vbnaish@yandex.ru>
2021-12-28 22:16:01 +0300

    Корректирует js код для таблицы

--- source/js/modules/rates-table.js
+++ source/js/modules/rates-table.js
@@ -1,21 +1,17 @@
 const WIDTH_TABLET = 660;
 const WIDTH_COLUMN_MOBILE = 280;
-const WIDTH_MARGIN_MOBILE = 20;
 
 const page = document.body;
 const blockRates = document.querySelector('.rates');
 
 
 if (blockRates && page.clientWidth < WIDTH_TABLET) {
-
   const tableRates = blockRates.querySelector('.rates__table');
   const buttonsRates = blockRates.querySelectorAll('.rates__button');
 
   tableRates.classList.remove('rates__table--nojs');
 
   const widthTableRates = buttonsRates.length*WIDTH_COLUMN_MOBILE;
-
-  tableRates.style.transform =`translateX(${((WIDTH_MARGIN_MOBILE - WIDTH_COLUMN_MOBILE)/widthTableRates*100)}%)`;
 
   const setActiveButtonsRates = (index) => {
     buttonsRates.forEach((buttonRates) => {
@@ -26,13 +22,11 @@ if (blockRates && page.clientWidth < WIDTH_TABLET) {
 
   buttonsRates.forEach((buttonRates, index) => {
     buttonRates.addEventListener('click', () => {
-      if (index === 0) {
-        tableRates.style.transform =`translateX(${(WIDTH_MARGIN_MOBILE/widthTableRates*100)}%)`;
-      } else {
-        tableRates.style.transform =`translateX(${((WIDTH_MARGIN_MOBILE - WIDTH_COLUMN_MOBILE*index)/widthTableRates*100)}%)`;
+      if (tableRates.classList.contains('rates__table--start')) {
+        tableRates.classList.remove('rates__table--start');
       }
+      tableRates.style.transform =`translateX(${(- WIDTH_COLUMN_MOBILE*index)/widthTableRates*100}%)`;
       setActiveButtonsRates(index);
     });
-
   });
 }
