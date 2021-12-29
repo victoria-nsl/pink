67e7ac7f2caba9198cfdfc4e88142cba3e4ff019
victoria-nsl <vbnaish@yandex.ru>
2021-12-29 15:18:00 +0300

    Устанавливает активную кнопку при сколле таблицы

--- build/js/modules/rates-table.js
+++ build/js/modules/rates-table.js
@@ -1,18 +1,17 @@
 const WIDTH_TABLET = 660;
-const WIDTH_COLUMN_MOBILE = 280;
 
 const page = document.body;
 const blockRates = document.querySelector('.rates');
 
-
 if (blockRates && page.clientWidth < WIDTH_TABLET) {
   const tableRates = blockRates.querySelector('.rates__table');
   const buttonsRates = blockRates.querySelectorAll('.rates__button');
+  const parentTableRates =  tableRates.offsetParent;
+  const numberScrolls = buttonsRates.length;
 
   tableRates.classList.remove('rates__table--nojs');
 
-  const widthTableRates = buttonsRates.length*WIDTH_COLUMN_MOBILE;
-
+  //функция, устанавливающая активную кнопку
   const setActiveButtonsRates = (index) => {
     buttonsRates.forEach((buttonRates) => {
       buttonRates.classList.remove('rates__button--active');
@@ -20,13 +19,31 @@ if (blockRates && page.clientWidth < WIDTH_TABLET) {
     buttonsRates[index].classList.add('rates__button--active');
   };
 
+  //изменение положение  таблицы при нажатии на кнопки
   buttonsRates.forEach((buttonRates, index) => {
     buttonRates.addEventListener('click', () => {
+      const widthTable = tableRates.offsetWidth;
       if (tableRates.classList.contains('rates__table--start')) {
         tableRates.classList.remove('rates__table--start');
       }
-      tableRates.style.transform =`translateX(${(- WIDTH_COLUMN_MOBILE*index)/widthTableRates*100}%)`;
+      parentTableRates.scrollLeft = Math.floor(widthTable * index/numberScrolls);
       setActiveButtonsRates(index);
     });
+  });
+
+  //изменение активной кнопки при скролле таблицы
+  parentTableRates.addEventListener('scroll', () => {
+    const widthTable = tableRates.offsetWidth;
+    const scrollDistance = parentTableRates.scrollLeft;
+
+    if (tableRates.classList.contains('rates__table--start')) {
+      tableRates.classList.remove('rates__table--start');
+    }
+
+    for (let i = 0; i < numberScrolls; i++) {
+      if (Math.floor(widthTable*i/numberScrolls) <= scrollDistance)  {
+        setActiveButtonsRates(i);
+      }
+    }
   });
 }
