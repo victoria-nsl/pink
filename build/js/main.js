efa2e231f702ff8f41ee19f70be0b58d627884a3
victoria-nsl <vbnaish@yandex.ru>
2022-01-09 13:07:58 +0300

    Добавляет маску телефона, валидацию в html файле

--- build/js/main.js
+++ build/js/main.js
@@ -5,3 +5,4 @@ import './modules/photo-settings.js';
 import './modules/photo-like.js';
 import './modules/slider-feedback.js';
 import './modules/rates-table.js';
+import './modules/mask-phone.js';
